f97328fc70b4ace1304ff0280779668c12a37bb8
Arina Babinskya <rianawilson94@gmail.com>
2026-04-21 09:56:26 +0300

    Delete extended_version directory

--- extended_version/level1.cpp
+++
@@ -1,9 +0,0 @@
-// at this point, we do not need realization of function - only names and types
-
-
-int add(int a, int b);
-float subtract(float a, float b);
-unsigned int multiply(unsigned int a, unsigned int b);
-double divide(double numerator, double denominator);
-bool logical_and(bool a, bool b);
-bool is_equal(long long a, long long b);
